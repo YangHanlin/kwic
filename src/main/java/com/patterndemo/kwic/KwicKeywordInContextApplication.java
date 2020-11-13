78471ffc94d5f1a9cd516be0319c828f3cca103d
Yang Hanlin <mattoncis@hotmail.com>
2020-11-13 14:33:59 +0800

    chore: initialize Spring project

---
+++ src/main/java/com/patterndemo/kwic/KwicKeywordInContextApplication.java
@@ -0,0 +1,13 @@
+package com.patterndemo.kwic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwicKeywordInContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwicKeywordInContextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patterndemo/kwic/KwicKeywordInContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patterndemo.kwic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwicKeywordInContextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
