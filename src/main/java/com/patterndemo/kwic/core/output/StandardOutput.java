c825a9cd821be52b6ad65d7aba19c86049255020
Yang Hanlin <mattoncis@hotmail.com>
2020-11-20 00:12:10 +0800

    refactor: add abstract class Output

--- src/main/java/com/patterndemo/kwic/core/output/StandardOutput.java
+++ src/main/java/com/patterndemo/kwic/core/output/StandardOutput.java
@@ -1,15 +1,6 @@
 package com.patterndemo.kwic.core.output;
 
-import com.patterndemo.kwic.core.piping.DataReceiver;
-import com.patterndemo.kwic.core.piping.State;
-import lombok.Getter;
-import lombok.Setter;
-
-public class StandardOutput implements DataReceiver<String> {
-
-    @Getter
-    @Setter
-    private State state;
+public class StandardOutput extends Output<String> {
 
     @Override
     public void putData(String data) {
