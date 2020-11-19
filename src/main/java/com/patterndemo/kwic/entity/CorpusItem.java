0683b2c4848f7f5a160c7525ebace19eb871edf6
Yang Hanlin <mattoncis@hotmail.com>
2020-11-19 15:11:29 +0800

    添加实体类

---
+++ src/main/java/com/patterndemo/kwic/entity/CorpusItem.java
@@ -0,0 +1,20 @@
+package com.patterndemo.kwic.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class CorpusItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+}
