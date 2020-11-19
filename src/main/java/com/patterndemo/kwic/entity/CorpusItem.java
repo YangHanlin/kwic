6b0a81477200a1343ce88d26d752348c3031852b
Yang Hanlin <mattoncis@hotmail.com>
2020-11-19 19:19:47 +0800

    feat: update entity classes

--- src/main/java/com/patterndemo/kwic/entity/CorpusItem.java
+++ src/main/java/com/patterndemo/kwic/entity/CorpusItem.java
@@ -2,19 +2,22 @@ package com.patterndemo.kwic.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name = "corpus")
 public class CorpusItem {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "batch_id")
+    private String batchId;
+
+    @Column(name = "content")
     private String content;
 
 }
