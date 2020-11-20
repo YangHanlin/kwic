f58ccd646c9577e53f0826d02d07f58d53ba7bcd
Yang Hanlin <mattoncis@hotmail.com>
2020-11-20 13:39:48 +0800

    chore: change base api url

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // Empty JS for your own code to be here
 
-const base_url = 'http://127.0.0.1:8080';
+// const base_url = 'http://127.0.0.1:8080';
+const base_url = 'https://api.tree-diagram.site/kwic';
 
 let request = null;
 let currentContents = null;
